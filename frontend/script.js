dd94dd13a3ae06051ccd30d8f2f552008771252f
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-04-21 22:12:28 +0800

    update

--- frontend/script.js
+++ frontend/script.js
@@ -3,5 +3,5 @@ let chicken = document.getElementById("chicken");
 fetch("https://messaging-app-backend-production-cd06.up.railway.app/messages")
   .then(res => res.json())
   .then(data => {
-    chicken.textContent = data.join("");
+    chicken.textContent = data;
   });
