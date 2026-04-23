f19547fc20b3276947f862afd4979ab0af26ee09
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-04-23 17:53:04 +0800

    update

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,17 @@ let inputpassword = document.getElementById("inputpassword");
 let button = document.getElementById("button");
 
 async function request() {
-  let res = await fetch("https://messaging-app-backend-production-cd06.up.railway.app/test-email");
+  let res = await fetch("https://messaging-app-backend-production-cd06.up.railway.app/test-email", {
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+    email:inputemail.value,
+    password:inputpassword.value
+    })
+  });
+
   chicken.textContent = await res.json();
 }
 
