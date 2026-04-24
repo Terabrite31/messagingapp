fbe7c74a473a97d271f1c01af4f48523bcfba81e
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-04-24 09:47:12 +0800

    update

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,14 @@ let inputemail = document.getElementById("inputemail");
 let inputpassword = document.getElementById("inputpassword");
 let button = document.getElementById("button");
 
+let inputverifaction = document.getElementById("inputverification");
+inputverification.style.display = "none";
+let verify = document.getElementById("verify");
+verify.style.display = "none";
+let verification = document.getElementById("verification");
+verification.style.display = "none";
+
+
 async function request() {
   let res = await fetch("https://messaging-app-backend-production-cd06.up.railway.app/test-email", {
     method:"POST",
@@ -16,6 +24,9 @@ async function request() {
   });
 
   chicken.textContent = await res.json();
+  inputverification.style.display = "block";
+  verify.style.display = "block";
+  verification.style.display = "block";
 }
 
 button.onclick = request;
