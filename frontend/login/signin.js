1fccf981d59c473074329895e663c5878c917e88
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-04-25 16:29:52 +0800

    push

---
+++ frontend/login/signin.js
@@ -0,0 +1,26 @@
+let inputemail = document.getElementById("inputemail");
+let inputpassword = document.getElementById("inputpassword");
+let button = document.getElementById("button");
+
+async function sendData() {
+    let res = await fetch("https://messaging-app-backend-production-cd06.up.railway.app/loginapi", {
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+        name: inputemail.value,
+        password: inputemailpassword.value
+    })
+
+    })
+    let data = await res.json()
+
+    if (data == "correct") {
+        localStorage.setItem("userEmail", inputemail.value);
+        window.location.href = "/frontend/messagingui/messaging.html";
+    }
+}
+
+button.onclick = sendData;
+
