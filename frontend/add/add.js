71e638a56f28bdf807804061b5fb51813c251288
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-05-06 16:44:58 +0800

    update

--- frontend/add/add.js
+++ frontend/add/add.js
@@ -16,7 +16,7 @@ r6.style.display = "none";
 let r7 = document.getElementById("r7");
 r7.style.display = "none";
 
-let email = localStorage.getItem("userEmail");
+let email = localStorage.getItem("token");
 
 
 
@@ -28,7 +28,7 @@ headers: {
     "Content-Type":"application/json"
 },
 body:JSON.stringify({
-    email: email,
+    token: token,
     REmail: inputemail.value
 })
 })
