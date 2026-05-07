61b7136ea68e1d39a7f005144943fe412a475f94
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-05-07 17:09:18 +0800

    update

--- frontend/messagingui/messaging.js
+++ frontend/messagingui/messaging.js
@@ -1,4 +1,54 @@
 let name = document.getElementById("name");
+let namediv = document.getElementById("namediv");
+let logout = document.getElementById("logout");
+logout.style.display = "none";
+
+let num = 0;
+
+function namee() {
+    num += 1;
+  if (num == 1) {
+    namediv.style.height = "150px";
+    logout.style.display = "block";
+
+  } else {
+    namediv.style.height = "50px";
+    logout.style.display = "none";
+  }
+  if (num == 2) {
+    num = 0;
+  }
+  
+  console.log(num)
+}
+
+
+
+
+name.onclick = namee;
+
+
+
+async function logoutt() {
+    let resp = fetch("https://messaging-app-backend-production-cd06.up.railway.app/logout", {
+  method: "GET",
+  credentials: "include"
+
+    })
+    let data = await resp.json();
+if (data == "logged out") {
+    window.location.href = "/frontend/login/signin.html";
+}
+}
+
+
+logout.onclick = logoutt;
+
+
+
+
+
+
 
 
 
