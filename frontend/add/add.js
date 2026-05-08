2166cc55455443d4721d42cf95604ac17794c001
Jason Torrevillas <jason@Jasons-MacBook-Air.local>
2026-05-09 07:26:27 +0800

    update

--- frontend/add/add.js
+++ frontend/add/add.js
@@ -2,9 +2,27 @@ let inputemail = document.getElementById("inputemail");
 let button = document.getElementById("button");
 let status = document.getElementById("status");
 let right = document.getElementById("right");
-
-
 let token = localStorage.getItem("token");
+const div = document.createElement("div")
+  const p = document.createElement("p")
+  const acceptBtn = document.createElement("button")
+  const declineBtn = document.createElement("button")
+
+  div.className = "requestdiv"
+  p.className = "requestemail"
+  acceptBtn.className = "acceptbtn"
+  declineBtn.className = "rbutton"
+
+  p.textContent = "gay"
+  acceptBtn.textContent = "Accept"
+  declineBtn.textContent = "Decline"
+
+  div.appendChild(p)
+  div.appendChild(acceptBtn)
+  div.appendChild(declineBtn)
+
+  right.appendChild(div)
+
 
 
 
@@ -47,28 +65,16 @@ credentials: "include"
 let data = await res.json();
 let sender = data.data;
 
-for (let i = 0; i < sender.length; i++) {
-  const div = document.createElement("div")
-  const p = document.createElement("p")
-  const acceptBtn = document.createElement("button")
-  const declineBtn = document.createElement("button")
 
-  p.textContent = sender[i].sender
-  acceptBtn.textContent = "Accept"
-  declineBtn.textContent = "Decline"
-
-  div.appendChild(p)
-  div.appendChild(acceptBtn)
-  div.appendChild(declineBtn)
-
-  right.appendChild(div)
-}
 
 console.log(sender[0].sender)
 
 }
 
 accept();
+
+
+
 
 
 
